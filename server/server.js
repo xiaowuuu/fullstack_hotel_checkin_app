8cbefd7c3051c13ed64aa4243708ce85385c085d
ying <huaiy1105@gmail.com>
2023-09-01 23:41:05 +0100

    setup server.js

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+// const createRouter = require("./helpers/create_router");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+MongoClient.connect("mongodb://127.0.0.1:27017",
+{useUnifiedTopology: true}).then((client) => {
+    const database = client.db("booking_db");
+    const bookings = database.collection("bookings");
+    // const bookingRouter = createRouter(bookings);
+    app.use("/api/bookings", bookingRouter)
+})
+.catch(console.error);
+
+app.listen(9000, function() {
+    console.log(`Listening on port ${this.address().port}`);
+})
