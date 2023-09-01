46841b5bcb2c426260c7f5edd18f434adcd4c5f8
ying <huaiy1105@gmail.com>
2023-09-01 23:06:38 +0100

    set up seeds

---
+++ server/db/seeds.js
@@ -0,0 +1,25 @@
+use booking_db;
+db.dropDatabase();
+
+db.bookings.insertMany([
+    {
+        guest_name: "Ying",
+        guest_email:"ying123@gmail.com",
+        checkin_status: false
+    },
+    {
+        guest_name: "Patrick",
+        guest_email:"patrick123@gmail.com",
+        checkin_status:True
+    },
+    {
+        guest_name: "Marc",
+        guest_email:"marc123@gmail.com",
+        checkin_status:True
+    },
+    {
+        guest_name: "Tom & Jerry",
+        guest_email:"tomjerry@gmail.com",
+        checkin_status: False
+    }
+]);
